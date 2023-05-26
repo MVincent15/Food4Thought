dade6275f75a2f716916379bd23774110d00e48d
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-05-25 19:43:32 -0500

    added starter code for js files

---
+++ public/js/addrecipe.js
@@ -0,0 +1,27 @@
+const blogPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#add-recipe-title').value.trim();
+    const ingredients = document.querySelector('#add-recipe-ingredients').value.trim();
+    const directions = document.querySelector('#add-recipe-directions').value.trim();
+  
+    if (title && ingredients && directions) {
+        // need to update fetch route once we establish routes
+      const response = await fetch('/', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+         // need to update route which this code will navigate to after successfull recipe add once we establish routes
+        document.location.replace("/");
+      } else {
+        alert('Failed to add blogpost.');
+      }
+    }
+  };
+
+  document
+  .querySelector('#add-blogpost')
+  .addEventListener('click', blogPostFormHandler);

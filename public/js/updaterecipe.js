bacb35902b87222fecba647620e8bec45f3deba1
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-06-01 18:47:50 -0500

    fixed bug where updating cook_time failed

--- public/js/updaterecipe.js
+++ public/js/updaterecipe.js
@@ -4,7 +4,7 @@ const updateRecipeFormHandler = async (event) => {
     const title = document.querySelector("#title-update").value.trim();
     const ingredients = document.querySelector("#ingredients-update").value.trim();
     const directions = document.querySelector("#directions-update").value.trim();
-    const cookTime = document.querySelector("#cooktime-update").value.trim();
+    const cook_time = document.querySelector("#cooktime-update").value.trim();
     const comments = document.querySelector("#comments-update").value.trim();
     const recipeId = window.location.pathname.split("/")[3];
   
@@ -12,7 +12,7 @@ const updateRecipeFormHandler = async (event) => {
       // need to update fetch route once we establish routes
       const response = await fetch(`/dashboard/updaterecipe/${recipeId}`, {
         method: "PUT",
-        body: JSON.stringify({ title, ingredients, directions, cookTime, comments }),
+        body: JSON.stringify({ title, ingredients, directions, cook_time, comments }),
         headers: { "Content-Type": "application/json" },
       });
   

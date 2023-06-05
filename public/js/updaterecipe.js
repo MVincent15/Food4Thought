3bedee76dd486c78af71363d9e0577da54d7e76a
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-06-05 16:26:01 -0500

    added code to utilize cloudinary for adding images to recipes

--- public/js/updaterecipe.js
+++ public/js/updaterecipe.js
@@ -6,13 +6,14 @@ const updateRecipeFormHandler = async (event) => {
     const directions = document.querySelector("#directions-update").value.trim();
     const cook_time = document.querySelector("#cooktime-update").value.trim();
     const comments = document.querySelector("#comments-update").value.trim();
+    const image_url = document.querySelector("#image-update").value.trim();
     const recipeId = window.location.pathname.split("/")[2];
   
     if (title && ingredients && directions) {
       // need to update fetch route once we establish routes
       const response = await fetch(`/updaterecipe/${recipeId}`, {
         method: "PUT",
-        body: JSON.stringify({ title, ingredients, directions, cook_time, comments }),
+        body: JSON.stringify({ title, ingredients, directions, cook_time, comments, image_url }),
         headers: { "Content-Type": "application/json" },
       });
   
